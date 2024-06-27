665dde9e09be74a02a5cbb710afb4455bd3a1734
Barry <argnz158cyber@gmail.com>
2024-06-27 15:20:13 +0700

    fix minor bug

--- lib/TimelineDetail.dart
+++ lib/TimelineDetail.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_helloo_world/Dashboard.dart';
 import 'package:flutter_helloo_world/Auth/login.dart';
-import 'package:flutter_helloo_world/Faq.dart';
 import 'package:flutter_helloo_world/History.dart';
 import 'package:flutter_helloo_world/Timeline.dart';
 
